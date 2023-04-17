8b89fed2597de8b1cad8ac522fdf817c11a0142e
Hergelink <enver.hergelink@gmail.com>
2023-04-17 13:38:31 +0300

    css changes

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 import Layout from '../components/Layout.jsx';
 import { GatsbyImage } from 'gatsby-plugin-image';
 import * as styles from '../styles/singleBlog.module.css';
 
 export default function BlogPost({ data }) {
   const post = data.markdownRemark;
+
+  const currentYear = new Date().getFullYear();
+  const yearsOfOperation = currentYear - 1989;
+
   return (
     <Layout>
       <div className={styles.singlePostContainer}>
@@ -20,6 +24,17 @@ export default function BlogPost({ data }) {
         )}
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+      <div className={styles.callToActionDiv}>
+        <h3>Bizimle Çalışın</h3>
+        <p>
+          Kurumsala çalışmak konusunda {yearsOfOperation} yıllık bir deneyime
+          sahibiz. Sizin dilinizi iyi bilen ekibimizle, yaklaşan
+          seyahatlerinizle ilgili tüm detayları incelikli biçimde, hızla çözer,
+          sorularınıza anında yanıt verir, öneriye ihtiyacınız olduğunda en
+          doğru seçenekleri sunarız.
+        </p>
+        <Link to='/iletisim' className={styles.callToActionBtn}>İletişim</Link>
       </div>
     </Layout>
   );
