1b5b9c6f6de56e53b46a7837e8f81ecba8163200
Enver Can Oude Hergelink <enver.hergelink@gmail.com>
2023-07-18 13:42:15 +0300

    changed head title

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -289,7 +289,7 @@ export default function Home() {
 }
 
 export function Head({ title, description }) {
-  const defaultTitle = 'Biletlazım.com - Kurumsal Seyahat Yazılımı';
+  const defaultTitle = 'Biletlazım.com - Kurumsal Seyahat Acentasız';
   const defaultDescription =
     'Uçak bileti, otel rezervasyonu, araç kiralama, vize ve sigorta işlemlerinden toplantı organizasyonuna kadar geniş bir hizmet yelpazesinde 1987 yılından itibaren hizmet gösteren kurumsal seyahat acentanız';
 
